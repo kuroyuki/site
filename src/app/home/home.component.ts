78d77b6303e0271d71c403ca31c08dc138638055
Yuri Zhukov <zhukov_yuri@ru.ibm.com>
2022-01-11 17:45:30 +0300

    remove 'three' to preserve minimal bundle size

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { EngineService } from '../engine.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  @ViewChild('rendererCanvas', {static: true})
-  public rendererCanvas: ElementRef<HTMLCanvasElement>;
+export class HomeComponent {
 
-  public constructor(private engServ: EngineService) {
-  }
-
-  public ngOnInit(): void {
-    this.engServ.createScene(this.rendererCanvas);
-    this.engServ.animate();
-  }
 }
